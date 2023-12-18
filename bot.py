e96624f1e755a7f40aeffc9ab3e49947a6b095eb
zckeyser <zckeyser@gmail.com>
2023-12-17 20:06:08 -0500

    feat: initial bot setup w/test command

--- bot.py
+++ bot.py
@@ -1,6 +1,18 @@
-# manage channels, send messages, read messages, use slash commands
-PERMISSIONS_BITMASK = 2147485712
+import disnake
+from disnake.ext import commands
+
+intents = disnake.Intents.default()
+intents.message_content = True
 
 with open("api_key.txt", "r") as f:
     TOKEN = f.read()
 
+from disnake.ext import commands
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.command()
+async def test(ctx, arg):
+    await ctx.send(arg)
+
+bot.run(TOKEN)
